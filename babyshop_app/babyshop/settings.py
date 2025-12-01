66f24add94d8dde880e766401231b684c20c9618
Ognjen Manojlovic <ogisha@MacBookPro.local>
2025-12-01 02:05:38 +0100

    Use DJANGO_ALLOWED_HOSTS environment variable

--- babyshop_app/babyshop/settings.py
+++ babyshop_app/babyshop/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-7j(@z8g0qc0hsl3wiqp55_ult3k3g&lh17@643@*g_q=sikrxr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    "DJANGO_ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
